30a5a8f2a65e0c0dec62c4dc545aaae97dc2889b
sw-programmer <swgger@kaist.ac.kr>
2025-03-24 20:14:47 +0900

    unk

--- inference.sh
+++ inference.sh
@@ -1,10 +1,18 @@
-DATA_DIR='original'
-DATA_SPLIT='valid'
-DATA_NUM=30
+DATA_DIR='augmented'
+DATA_SPLIT='test'
+DATA_NUM=100
 MODEL_NAME="gpt-4o"
 TEMPERATURE=0.6
 
 python -m inference --data_dir $DATA_DIR \
     --data_split $DATA_SPLIT \
+    --data_num $DATA_NUM \
     --model_name $MODEL_NAME \
     --temperature $TEMPERATURE \
+
+
+# TEMPERATURE=0.2
+# python -m inference --data_dir $DATA_DIR \
+#     --data_split $DATA_SPLIT \
+#     --model_name $MODEL_NAME \
+#     --temperature $TEMPERATURE \
